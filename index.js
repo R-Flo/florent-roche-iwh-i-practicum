c7519e581ce7bd212a86429795baaa59ca4ee05a
Florent Roche <florent@kaffein.agency>
2026-05-11 15:32:25 +0200

    Implement GET / route to list board games from custom object API

--- index.js
+++ index.js
@@ -1,71 +1,64 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const PORT = process.env.PORT || 3000;
+
+const CUSTOM_OBJECT_TYPE = '2-62299495';
+const CUSTOM_PROPERTIES = ['name', 'genre', 'min_players', 'max_players', 'play_time_minutes'];
+
+const HUBSPOT_API = 'https://api.hubapi.com';
+const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+};
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+app.get('/', async (req, res) => {
+    const url = `${HUBSPOT_API}/crm/v3/objects/${CUSTOM_OBJECT_TYPE}?properties=${CUSTOM_PROPERTIES.join(',')}&limit=100`;
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(url, { headers });
+        const games = response.data.results || [];
+        res.render('homepage', {
+            title: 'Board Game Collection | Integrating With HubSpot I Practicum',
+            games
+        });
     } catch (error) {
-        console.error(error);
+        console.error('GET / failed:', error.response ? error.response.data : error.message);
+        res.status(500).send('Failed to load board games. Check the server console.');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
+
+app.post('/update-cobj', async (req, res) => {
+    const payload = {
         properties: {
-            "favorite_book": req.body.newVal
+            name: req.body.name,
+            genre: req.body.genre,
+            min_players: req.body.min_players,
+            max_players: req.body.max_players,
+            play_time_minutes: req.body.play_time_minutes
         }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
     };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    const url = `${HUBSPOT_API}/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+    try {
+        await axios.post(url, payload, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('POST /update-cobj failed:', error.response ? error.response.data : error.message);
+        res.status(500).send('Failed to create board game. Check the server console.');
     }
-
 });
-*/
 
-
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
